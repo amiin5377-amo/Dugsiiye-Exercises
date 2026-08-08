b4b19498d3c700ea6f6e45cbe02f58accc4fe83b
amiin5377-amo <amiin5377@gmail.com>
2026-08-08 17:03:23 +0200

    first commit

---
+++ React Exercises/Exercise 3/src/App.jsx
@@ -0,0 +1,16 @@
+import Usercards from './Usercard'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Usercards name="John Doe" email="john.doe@example.com" /> 
+      <Usercards name="Jane Smith" email="jane.smith@example.com" /> 
+      <Usercards name="Joe Blow" email="joe.blow@example.com" /> 
+      <Usercards name="Jane Doe" email="jane.doe@example.com" /> 
+    </>
+  )
+}
+
+export default App
---
+++ React Exercises/Exercise 3/src/Usercards.jsx
@@ -0,0 +1,9 @@
+function Usercards({name, email}) {
+    return (
+        <div>
+            <h2>{name}</h2>
+            <p>{email}</p>
+        </div>
+    )
+}
+export default Usercards
